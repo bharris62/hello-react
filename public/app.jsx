b324e209ad4ef36940d928d9d745db9dce980cfa
Andrew Harris <BHarris@Andrews-MacBook-Pro.local>
2016-05-14 22:46:18 -0500

    Adding my first react commit

---
+++ public/app.jsx
@@ -0,0 +1,51 @@
+var Greeter = React.createClass({
+  getDefaultProps: function(){
+    return {
+      name: 'React',
+      message: "This is from the component"
+    };
+  },
+
+  getInitialState: function(){
+    return {
+      name: this.props.name
+    };
+  },
+
+  onButtonClick: function (e){
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value= '';
+
+    if (typeof name === 'string' && name.length > 0) {
+      this.setState({
+        name: name
+      });
+    }
+  },
+  render: function (){
+    var name = this.state.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello, {name}!</h1>
+        <p>{message}</p>
+
+      <form onSubmit={this.onButtonClick}>
+        <input type='text' ref='name'/>
+        <button>Set Name</button>
+      </form>
+      </div>
+    );
+  }
+});
+
+var name = 'Andrew';
+var message="THIS IS FROM THE COMPONENT!!!!"
+ReactDOM.render(
+  <Greeter name ={name}/>,
+  document.getElementById('app')
+);
